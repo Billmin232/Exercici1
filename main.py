3e25375cf5e707b330f4707c2d6b725436d6b54e
Billmin232 <ripollguille@gmail.com>
2023-10-03 09:51:10 +0200

    Main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
 
 a = random.randint(3, 10)
-print(a)
-i = 0
 z = 0
 e = 2.71828182846
 
@@ -14,12 +12,8 @@ for l in range(a):
     x.append(random.uniform(0, 1))
     w.append(random.randint(1, 10))
 
-for t in range(a):
-    print(x[t], w[t])
-
-while i < a:
+for i in range(a):
     v[i] = x[i] * w[i]
-    i = i + 1
 
 for j in v:
     z = z + j
