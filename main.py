c26fa8d698f80b079ee3e9e709d7c0b9ad1cf9a6
Billmin232 <ripollguille@gmail.com>
2023-10-17 09:35:51 +0200

    Començament de actualitzacio de pesos

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 class Neurona:
-    def __init__(self, inputs, pesos):
+    def __init__(self, inputs, pesos, Bias, fila):
         self.z = 0
         self.e = 2.71828182846
+        self.fila = fila
         self.nimp = len(inputs)
-        self.imputs = inputs
+        self.inputs = inputs
         self.pesos = pesos
+        self.bias = Bias
 
     def Calcular(self):
         v = []
-        self.pesos = [1] * self.nimp
 
         for i in range(self.nimp):
-            v.append(self.imputs[i] * self.pesos[i])
+            v.append(self.inputs[i] * self.pesos[self.fila][i])
 
         for i in v:
             self.z = self.z + i
@@ -19,28 +20,57 @@ class Neurona:
         result = self.sigmoid(self.z)
         return result
 
+    def LOL(self):
+        return self.z
+
     def sigmoid(self, z):
-        a = 1 / (1 + self.e ** -(z + 1))
+        a = (1 / (1 + self.e ** -(z + self.bias[self.fila])))
         return a
 
-
+LR = 0.25
 Entradas1 = [0, 1]
-Pesos = [1] * 12
-n1 = Neurona(Entradas1, Pesos)
-res1 = n1.Calcular()
+z1 = [0] * 3
+z2 = [0] * 2
+W1 = [[1, 1],
+      [1, 1],
+      [1, 1]]
 
-n2 = Neurona(Entradas1, Pesos)
-res2 = n2.Calcular()
+W2 = [[1, 1, 1],
+      [1, 1, 1]]
 
-n3 = Neurona(Entradas1, Pesos)
-res3 = n3.Calcular()
+Bias = [1, 1, 1]
 
-Entradas2 = [res1, res2, res3]
-n4 = Neurona(Entradas2, Pesos)
-res4 = n4.Calcular()
+n1 = Neurona(Entradas1, W1, Bias, 0)
+a1 = n1.Calcular()
+z1[0] = n1.LOL()
 
-n5 = Neurona(Entradas2, Pesos)
-res5 = n5.Calcular()
+n2 = Neurona(Entradas1, W1, Bias, 1)
+a2 = n2.Calcular()
+z1[1] = n2.LOL()
+
+n3 = Neurona(Entradas1, W1, Bias, 2)
+a3 = n3.Calcular()
+z1[2] = n3.LOL()
+
+Entradas2 = [a1, a2, a3]
+n4 = Neurona(Entradas2, W2, Bias, 0)
+a4 = n4.Calcular()
+z2[0] = n4.LOL()
+
+n5 = Neurona(Entradas2, W2, Bias, 1)
+a5 = n5.Calcular()
+z2[1] = n5.LOL()
+
+Salidas = [a4, a5]
 
 Salida = [1, 0]
-Err2 = res4 - Salida[0]
+Err2 = [a4 - Salida[0], a5 - Salida[1]]
+
+for i in range(2):
+    for j in range(3):
+        W2[i][j] = W2[i][j] - LR * (-Err2[i] * Salidas[i])
+
+for fila in W2:
+    for elemento in fila:
+        print(elemento, end=' ')
+    print()
